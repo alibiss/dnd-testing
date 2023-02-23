ab0afad9f51cf587ae089b43ed6642a5679b1c47
alibiss <silvioalbi@outlook.com>
2023-02-23 01:56:24 +0100

    Refactor code and add shit+click to delete item from hotbar

--- app.js
+++ app.js
@@ -10,36 +10,32 @@ for (let i=0; i<24; i++) {
     action.setAttribute("draggable", "true");
     action.style.backgroundColor = color;
     list.appendChild(action);
+    initItem(action);
 }
 
-// Retrieve info on what skill is being dragged
-document.addEventListener("dragstart", (e) => {
-    e.dataTransfer.clearData();
-    e.dataTransfer.setData("name", e.target.getAttribute("data-skill"));
-    e.dataTransfer.setData("slot", e.target.parentNode.getAttribute("data-slot") || "empty");
-    e.dataTransfer.setData("css", e.target.getAttribute("style"));
-});
+document.querySelectorAll("#slots > .container").forEach(node => {
+    // Shift click to remove element
+    node.addEventListener("click", (e) => {
+        e.preventDefault();
 
-// No clue why this is needed..
-document.addEventListener("dragover", (e) => { e.preventDefault() });
+        if ( !e.shiftKey ) return;
+        if ( e.target.classList.contains("item") ) e.target.remove();
+    })
 
-// Find out if drop location is valid or not
-document.addEventListener("drop", (e) => {
-    e.preventDefault();
+    // Set .container nodes as valid drop zones
+    node.addEventListener("dragover", (e) => { e.preventDefault() });
+    
+    node.addEventListener("drop", (e) => {
+        // Parse payload's content
+        const action = JSON.parse(e.dataTransfer.getData("Payload"));
 
-    const action = {
-        name: e.dataTransfer.getData("name"),
-        slot: e.dataTransfer.getData("slot"),
-        style: e.dataTransfer.getData("css")
-    };
+        if ( action.slot === "empty" ) {
+            // If dragging an action from the list
 
-    if ( action.slot === "empty" ) {
-        // If dragging an action from the list
-        if ( e.target.classList.contains("container") || e.target.classList.contains("child") ) {
             let destination = e.target;
-            if ( e.target.classList.contains("child") ) {
+            if ( e.target.classList.contains("item") ) {
                 // If slot already filled, remove sibling first
-                const sibling = e.target.parentNode.querySelector("[data-skill]");
+                const sibling = e.target.parentNode.querySelector(".item");
                 destination = e.target.parentNode;
                 sibling.remove();
             }
@@ -50,22 +46,30 @@ document.addEventListener("drop", (e) => {
             item.setAttribute("draggable", "true");
             item.style = action.style;
             destination.appendChild(item);
-        }
-    } else {
-        // If dragging an action from the hotbars
-        const node = document.querySelector(`[data-slot="${action.slot}"] > [data-skill="${action.name}"]`);
-
-        if ( e.target.classList.contains("container") ) {
-            e.target.appendChild(node);
-        } else if ( e.target.classList.contains("child") ) {
-            const sibling = e.target.parentNode.querySelector("[data-skill]"),
-                destination = document.querySelector(`[data-slot="${action.slot}"]`);
-    
-            e.target.parentNode.appendChild(node);
-            destination.appendChild(sibling);
+            initItem(item);
         } else {
-            // ..to invalid space
-            node.remove();
+            // If dragging an action from the hotbars
+
+            const node = document.querySelector(`[data-slot="${action.slot}"] > [data-skill="${action.name}"]`);
+            if ( e.target.classList.contains("container") ) {
+                e.target.appendChild(node);
+            } else if ( e.target.classList.contains("item") ) {
+                const sibling = e.target.parentNode.querySelector(".item"),
+                    destination = document.querySelector(`[data-slot="${action.slot}"]`);
+        
+                e.target.parentNode.appendChild(node);
+                destination.appendChild(sibling);
+            }
         }
-    }
-});
+    })
+})
+
+function initItem(i) {
+    i.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("Payload", JSON.stringify({
+            name: e.target.getAttribute("data-skill"),
+            slot: e.target.parentNode.getAttribute("data-slot") || "empty",
+            style: e.target.getAttribute("style")
+        }));
+    })
+}
